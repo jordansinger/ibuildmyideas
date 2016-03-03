eb1e36387965f7259f0197b782053a3f7608726a
Jordan Singer <singer.jordan@gmail.com>
2016-03-03 16:29:15 -0700

    Update Google Analytics

--- index.php
+++ index.php
@@ -7,20 +7,16 @@
     <meta name="description" content="I build my ideas." />
     <meta name="robots" content="index, follow" />
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-20832881-5']);
-        _gaq.push(['_trackPageview']);
+    <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        (function () {
-            var ga = document.createElement('script');
-            ga.type = 'text/javascript';
-            ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(ga, s);
-        })();
-    </script>
+	  ga('create', 'UA-20832881-8', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 	<style type="text/css">
 		@import url(https://fonts.googleapis.com/css?family=Roboto:300,500);
 		* {

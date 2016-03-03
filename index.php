0d3d8e0d5a6a1949eba2e82a57246dc457f74a01
Jordan Singer <singer.jordan@gmail.com>
2016-03-03 16:22:29 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="minimal-ui,width=device-width,user-scalable=no" />
+    <title>Jordan Singer</title>
+    <meta name="description" content="I build my ideas." />
+    <meta name="robots" content="index, follow" />
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-20832881-5']);
+        _gaq.push(['_trackPageview']);
+
+        (function () {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+	<style type="text/css">
+		@import url(https://fonts.googleapis.com/css?family=Roboto:300,500);
+		* {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+			text-decoration: none;
+			outline: none;
+			border: none;
+			outline: none;
+			-webkit-font-smoothing: antialiased;
+			-moz-osx-font-smoothing: grayscale;
+			box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			-webkit-box-sizing: border-box;
+		}
+		body {
+			font-family: "Roboto", "Helvetica Neue", Helvetica, Arial, sans-serif;
+			padding: 64px;
+		}
+		ul {
+			width: auto;
+			margin: 0 auto;
+		}
+		ul li {
+			font-size: 23px;
+			line-height: 64px;
+			font-weight: 300;
+		}
+		ul li.title {
+			font-weight: 500;
+		}
+		ul li a {
+			color: rgba(0, 0, 0, 0.64);
+			font-size: 20px;
+		}
+		ul li a:hover {
+			text-decoration: underline;
+		}
+		@media screen and (max-width: 400px) {
+			body {
+				padding: 48px;
+			}
+		}
+	</style>
+</head>
+<body>
+	<ul>
+		<li class="title">I build my ideas.</li>
+		<li><a href="http://onlyhandle.com/jordan">[jordan]</a></li>
+		<br>
+		<li><a href="http://represent.io">Represent</a></li>
+		<li><a href="http://shipp.co">Shipp</a></li>
+		<li><a href="http://overboard.me">Overboard</a></li>
+		<li><a href="http://startconvoy.com">Convoy</a></li>
+		<li><a href="https://balloon.io">Balloon</a></li>
+		<li><a href="http://buck.io">Buck</a></li>
+		<li><a href="http://onlyhandle.com">Handle</a></li>
+		<li><a href="https://www.trymask.com">Mask</a></li>
+		<li><a href="http://haggle.io">Haggle</a></li>
+		<li><a href="http://realmoji.com">Realmoji</a></li>
+		<li><a href="https://github.com/jordansinger/hook.js">Hook.js</a></li>
+		<li><a href="http://yohook.herokuapp.com">YoHook</a></li>
+		<li><a href="http://whistlapp.com">Whistl</a></li>
+		<li><a href="http://getbonfireapp.com">Bonfire</a></li>
+	</ul>
+</body>
+</html>

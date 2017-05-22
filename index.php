22be719374bd673e59a4c4b674ea8a40078d2609
Jordan Singer <singer.jordan@gmail.com>
2017-05-21 20:24:30 -0700

    website updates

--- index.php
+++ index.php
@@ -17,73 +17,172 @@
 	  ga('send', 'pageview');
 
 	</script>
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css">
 	<style type="text/css">
 		* {
 			margin: 0;
 			padding: 0;
-			list-style: none;
-			text-decoration: none;
-			outline: none;
-			border: none;
-			outline: none;
-			-webkit-font-smoothing: antialiased;
-			-moz-osx-font-smoothing: grayscale;
-			box-sizing: border-box;
-			-moz-box-sizing: border-box;
-			-webkit-box-sizing: border-box;
 		}
-		body {
-			font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+		html, body {
+			width: 100%;
+			height: 100%;
+			font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+		}
+		#intro {
+			position: relative;
+			width: 100%;
+			height: 100%;
+			display: table;
+			text-align: center;
+			overflow: hidden;
+			background: #2a2a2a;
+		}
+		#intro .word {
+			font-size: 48px;
+			font-weight: 700;
+			color: #fff;
+			display: table-cell;
+		    vertical-align: middle;
+		    cursor: default;
+		}
+		#main {
+			display: none;
 			padding: 64px;
 		}
 		ul {
 			width: auto;
 			margin: 0 auto;
+			list-style: none;
 		}
 		ul li {
-			font-size: 23px;
+			font-size: 24px;
 			line-height: 64px;
-			font-weight: 400;
+			font-weight: 500;
+			color: #000;
 		}
 		ul li.title {
-			font-weight: 500;
+			font-weight: 700;
 		}
 		ul li a {
 			color: rgba(0, 0, 0, 0.64);
 			font-size: 20px;
+			text-decoration: none;
 		}
 		ul li a:hover {
 			text-decoration: underline;
 		}
 		@media screen and (max-width: 400px) {
-			body {
+			#intro .word {
+				font-size: 32px;
+			}
+			#main {
 				padding: 48px;
 			}
 		}
 	</style>
 </head>
 <body>
-	<ul>
-		<li class="title">I build my ideas.</li>
-		<li><a href="http://onlyhandle.com/jordan">[jordan]</a></li>
-		<br>
-		<li><a href="http://represent.io">Represent</a></li>
-		<li><a href="https://balloon.io">Balloon</a></li>
-		<li><a href="https://botlytics.co">Botlytics</a></li>
-		<li><a href="https://buck.io">Buck</a></li>
-		<li><a href="https://taste-kit.com">TasteKit</a></li>
-		<li><a href="https://wonder-bot.com">Wonder</a></li>
-		<li><a href="http://startconvoy.com">Convoy</a></li>
-		<li><a href="http://overboard.me">Overboard</a></li>
-		<li><a href="https://trymask.com">Mask</a></li>
-		<li><a href="http://onlyhandle.com">Handle</a></li>
-		<li><a href="https://github.com/jordansinger/hook">Hook</a></li>
-		<li><a href="http://tryoutreach.com">Outreach</a></li>
-		<li><a href="http://realmoji.com">Realmoji</a></li>
-		<li><a href="https://haggleio.herokuapp.com">Haggle</a></li>
-		<li><a href="http://yohook.herokuapp.com">YoHook</a></li>
-		<li><a href="http://whistlapp.com">Whistl</a></li>
-		<li><a href="http://shipp.herokuapp.com">Shipp</a></li>
-	</ul>
+	<div id="intro" class="animated">
+		<div class="word animated"></div>
+	</div>
+
+	<div id="main" class="animated">
+		<ul>
+			<li class="title">I build my ideas.</li>
+			<li><a href="http://onlyhandle.com/jordan">[jordan]</a></li>
+			<br>
+			<li><a href="http://represent.io">Represent</a></li>
+			<li><a href="https://wonder-bot.com">Wonder</a></li>
+			<li><a href="https://askplaybook.com">Playbook</a></li>
+			<li><a href="https://balloon.io">Balloon</a></li>
+			<li><a href="https://botlytics.co">Botlytics</a></li>
+			<li><a href="https://thewdaily.com">The W</a></li>
+			<li><a href="https://buck.io">Buck</a></li>
+			<li><a href="https://taste-kit.com">TasteKit</a></li>
+			<li><a href="http://startconvoy.com">Convoy</a></li>
+			<li><a href="http://overboard.me">Overboard</a></li>
+			<li><a href="https://trymask.com">Mask</a></li>
+			<li><a href="http://onlyhandle.com">Handle</a></li>
+			<li><a href="https://github.com/jordansinger/hook">Hook</a></li>
+			<li><a href="http://tryoutreach.com">Outreach</a></li>
+			<li><a href="http://realmoji.com">Realmoji</a></li>
+			<li><a href="https://haggleio.herokuapp.com">Haggle</a></li>
+			<li><a href="http://yohook.herokuapp.com">YoHook</a></li>
+			<li><a href="http://whistlapp.com">Whistl</a></li>
+			<li><a href="http://shipp.herokuapp.com">Shipp</a></li>
+		</ul>
+	</div>
+
+	<script type="text/javascript">
+		var words = "I build my ideas.";
+		words = words.split(" ");
+		var word_element = $("#intro .word");
+		var projects = [
+			"Represent",
+			"Wonder",
+			"Playbook",
+			"Balloon",
+			"Botlytics",
+			"The W",
+			"Buck",
+			"TasteKit",
+			"Convoy",
+			"Overboard",
+			"Mask",
+			"Handle",
+			"Hook",
+			"Outreach",
+			"Realmoji",
+			"Haggle",
+			"YoHook",
+			"Whistl",
+			"Shipp"
+		];
+
+		function show_projects(show) {
+			if(show) {
+				setTimeout(function(){
+					$.each(projects, function(i, word) {
+						setTimeout(function(){
+						  word_element.text(word);
+						  show_main(projects.length - 1 == i);
+						}, (350 * i) / 2);
+					});
+				}, 1350);
+			}
+		}
+
+		function show_main(show) {
+			if(show) {
+				setTimeout(function(){
+					$("#intro").addClass("fadeOut").hide();
+					$("#main").show().addClass("fadeIn");
+				}, 1350);
+			}
+		}
+
+		$(document).ready(function() {
+			$.each(words, function(i, word) {
+				setTimeout(function(){
+				  word_element.text(word);
+				  switch(i) {
+				  	case 0:
+				  		word_element.addClass("fadeInDown");
+				  		break;
+				  	case 1:
+				  		word_element.removeClass("fadeInDown").addClass("fadeInRight");
+				  		break;
+				  	case 2:
+				  		word_element.removeClass("fadeInRight").addClass("fadeInUp");
+				  		break;
+				  	case 3:
+				  		word_element.removeClass("fadeInUp").addClass("fadeInLeft");
+				  		break;
+				  }
+				  show_projects(words.length - 1 == i);
+				}, 1000 * i);
+			});
+		});
+	</script>
 </body>
 </html>
